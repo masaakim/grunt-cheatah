ee371529424fbcc05cf69dc21cf7c7fff369a35b
Masaaki Morishita <ikasama3.0@gmail.com>
2014-06-15 02:56:49 +0900

    write Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 /*
  * grunt-cheatah
- * 
+ *
  *
  * Copyright (c) 2014 Masaaki Morishita
  * Licensed under the MIT license.
@@ -9,10 +9,8 @@
 'use strict';
 
 module.exports = function (grunt) {
-  // load all npm grunt tasks
   require('load-grunt-tasks')(grunt);
 
-  // Project configuration.
   grunt.initConfig({
     jshint: {
       all: [
@@ -26,46 +24,22 @@ module.exports = function (grunt) {
       }
     },
 
-    // Before generating any new files, remove any previously-created files.
     clean: {
       tests: ['tmp']
     },
 
-    // Configuration to be run (and then tested).
     cheatah: {
-      default_options: {
-        options: {
-        },
+      options: {
         files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!'
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
+          'tmp/cheatah.html': ['test/fixtures.css']
         }
       }
-    },
-
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
     }
-
   });
 
-  // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
   grunt.registerTask('test', ['clean', 'cheatah', 'nodeunit']);
-
-  // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
 
 };
